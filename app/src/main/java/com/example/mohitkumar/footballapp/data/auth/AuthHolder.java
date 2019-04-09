8f5e112523f8adb0a2dd8ee5e1f067c53bd78c64
batbrain7 <mohitkumar2k15@dtu.ac.in>
2019-04-09 18:25:37 +0530

    feat: add viewpager in mainactivity

--- app/src/main/java/com/example/mohitkumar/footballapp/data/auth/AuthHolder.java
+++ app/src/main/java/com/example/mohitkumar/footballapp/data/auth/AuthHolder.java
@@ -5,6 +5,6 @@ import com.example.mohitkumar.footballapp.BuildConfig;
 public class AuthHolder {
 
     public String getAuthorization() {
-        return BuildConfig.AUTH_CODE;
+        return "";
     }
 }
